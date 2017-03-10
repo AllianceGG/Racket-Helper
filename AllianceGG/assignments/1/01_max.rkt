23782ec784f332a7ac8855a203955db286addd8b
dzenyu <dzenyu.s@gmail.com>
2017-03-10 16:06:30 +0800

    optimize some codes

--- AllianceGG/assignments/1/01_max.rkt
+++ AllianceGG/assignments/1/01_max.rkt
@@ -1,12 +1,10 @@
 #lang racket
 [define [myloop]
   [define [mymax cur left]
+    ; since the first num has been read in, the line stops when "left = 1"
     [if [= 1 left] 
-      ; since the first num has been read in, the line stops when "left = 1"
       [displayln cur]
-      [let [
-            [next [read]]]
-        [mymax [max cur next] [- left 1]]]]]
+      [mymax [max cur [read]] [- left 1]]]]
   [let [
         [n [read]]
         [first [read]]] ; read the first num to trigger comparison
--- AllianceGG/assignments/1/06_fast_exp_iter.rkt
+++ AllianceGG/assignments/1/06_fast_exp_iter.rkt
@@ -1,42 +1,9 @@
 #lang racket
-
-; first, deal with trivial cases of 0^0, 0^n, n^0, n^1
-; the power's binary format shows how to get the result by iteration
-;
-; example:
-; the following recursive way
-; x^77 <- x^76 <- x^38 <- x^19 <- x^18 <- x^9 <- x^8 <- x^4 <- x^2 <- x^1
-; can be convertde into iteration with the help of 77's binary format of 1001101
-; in detail,
-; 1 the first bit always means sqr (x^1 -> x^2)
-; 0 sqr (x^2 -> x^4)
-; 0 sqr (x^4 -> x^8)
-; 1 self-multiplication and then sqr ( (x^8 * x)^2 = x^18 )
-; 1 self-multiplication and then sqr ( (x^18 * x)^2 = x^38 )
-; 0 sqr (x^38 -> x^76)
-; 1 the last bit means whether to have another self-multiplication (x^76 -> x^77)
-;
-; int->rBi: convert into binary format in REVERSED order.
-; fast-exp-iter: the priming function call's postion is -1 since the very first 
-;                calculation is always sqr(x^1). so -1 just means this case and 
-;                triggers the following process.
-
-[define [fast-exp base power]
-  [define [int->rBi num rBi]
-    [if [= 0 num]
-      rBi
-      [int->rBi [quotient num 2] [append rBi [list [remainder num 2]]]]]]
-  [define [fast-exp-iter base sofar rBi cur]
-    [cond
-      [[= cur -1] [fast-exp-iter base [sqr sofar] rBi [- [length rBi] 2]]]
-      [[= cur 0] [if [= [list-ref rBi 0] 0] sofar [* sofar base]]]
-      [else [if [= [list-ref rBi cur] 0] [fast-exp-iter base [sqr sofar] rBi [- cur 1]]
-              [fast-exp-iter base [sqr [* sofar base]] rBi [- cur 1]]]]]]
+[define [fast-exp base power ans]
   [cond
-    [[= 0 base] [if [= 0 power] [error "0^0 no solution"] [displayln 0]]]
-    [[= 0 power] [displayln 1]]
-    [[= 1 power] [displayln base]]
-    [else [displayln [fast-exp-iter base base [int->rBi power empty] -1]]]]]
+    [[= 0 power] ans]
+    [[even? power] [fast-exp [sqr base] [/ power 2] ans]]
+    [else [fast-exp base [- power 1] [* ans base]]]]]
 
 [define [myloop]
   [let [
@@ -46,7 +13,9 @@
     [if [eq? a eof]
         [void]
         [begin
-          [fast-exp a b]
+          [if [and [= 0 a] [= 0 b]]
+            [displayln "no answer"]
+            [displayln [fast-exp a b 1]]]
           [myloop]]]]]
 
 [myloop]
